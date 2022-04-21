3304088a3be95661d0d84ab67fc6fcbed9153d0d
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 08:52:14 -0500

    I created the MissionCommander class and it was instantiated in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander')
+
+const carlo = new MissionCommander("Carlo Gilmar")
+console.log(carlo.name)
+
+const martin = new MissionCommander("Martín González")
+console.log(martin.name)
+
+const juan = new MissionCommander("Juan Pérez")
+console.log(juan.name)
