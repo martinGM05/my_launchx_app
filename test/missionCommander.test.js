cd6639734c5edf6500cbe2b50ef76ae98d53a7ea
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 09:13:21 -0500

    Add Documentation

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -3,6 +3,6 @@ const MissionCommander = require('../app/missionCommander')
 describe("Unit test for Mission Commander", () => {
     test('1) Create a mission commander object', () => {
         const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe("Woopa")
+        expect(myMissionCommander.name).toBe("Woopaa")
     });
 })
